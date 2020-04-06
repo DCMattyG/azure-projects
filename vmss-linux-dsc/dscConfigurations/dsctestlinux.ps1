bab56f8681d3a3876af1c27955632e1edde8b862
Matthew Garrett <magarret@microsoft.com>
2020-04-06 02:01:59 -0700

    Added Nginx to PS script

--- vmss-linux-dsc/dscConfigurations/dsctestlinux.ps1
+++ vmss-linux-dsc/dscConfigurations/dsctestlinux.ps1
@@ -1,15 +1,59 @@
-﻿Configuration dsctestlinux{
+﻿$IndexPage = @'
+<html>
+<head>
+<title>My DSC Page</title>
+</head>
+<body>
+<H1>Awesome DSC Test Page in the house!</H1>
+</body>
+</html>
+'@
 
-  Import-DscResource -Module PSDesiredStateConfiguration
-  Import-DscResource -Module nx
+Configuration dsctestlinux {
+
+    Import-DscResource -Module PSDesiredStateConfiguration
+    Import-DscResource -Module nx
   
-    Node  "TestDSCLinuxfile"{
-    nxFile ExampleFile {
-      DestinationPath = "/tmp/example"
-      Contents = "hello world `n"
-      Ensure = "Present"
-      Type = "File"
+    Node  "TestDSCLinuxfile" {
+        nxFile ExampleFile {
+          DestinationPath = "/tmp/example"
+          Contents = "hello world `n"
+          Ensure = "Present"
+          Type = "File"
+        }
     }
-  }
-  }
-  
+
+    Node "dscwebserver" {
+        nxPackage nginx {
+            Name = "nginx"
+            Ensure = "Present"
+            PackageManager = "apt"
+        }
+
+        nxFile index_html {
+            DestinationPath = "/var/www/html/index.html"
+            Type = "file"
+            Contents = $IndexPage
+            DependsOn = "[nxPackage]nginx"
+        }
+
+        nxService nginxservice {
+            Name = "nginx"
+            State = "running"
+            Enabled = $true
+            Controller = "systemd"
+            DependsOn = "[nxFile]index_html"
+        }
+
+        nxFirewall FWConfig {
+            Name = "Allow Nginx HTTP"
+            InterfaceName = "eth0" 
+            FirewallType = "firewalld"
+            Ensure = "Present"
+            Access = "Allow"
+            Direction = "Input"
+            DestinationPort = "80"
+            Position = "Before-End"
+       }
+    }
+}

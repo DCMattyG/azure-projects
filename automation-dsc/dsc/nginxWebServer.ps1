cc1b54ab22ced6855204a285aab49e5934ad11c4
dcmattyg <mgarrett0402@gmail.com>
2020-04-25 23:45:37 -0700

    Added powershell parameters and a test to verify functionality

--- automation-dsc/dsc/nginxWebServer.ps1
+++ automation-dsc/dsc/nginxWebServer.ps1
@@ -10,61 +10,90 @@
 "@
 
 Configuration linuxconfig {
+  param
+  (
+    [parameter(Mandatory)]
+    [String]
+    $fileShareURL,
+
+    [parameter(Mandatory)]
+    [String]
+    $fileShareKey
+  )
 
   Import-DscResource -Module PSDesiredStateConfiguration
   Import-DscResource -Module nx
   # Import-DscResource -Module nxNetworking
-  
-  Node "TestLinuxfile" {
+
+  # Node "TestLinuxfile" {
+  #   nxFile ExampleFile {
+  #     DestinationPath = "/tmp/example"
+  #     Contents = "hello world `n"
+  #     Ensure = "Present"
+  #     Type = "File"
+  #   }
+  # }
+
+  Node "NginxWebServer" {
     nxFile ExampleFile {
       DestinationPath = "/tmp/example"
-      Contents = "hello world `n"
+      Contents = "URL: $($fileShareURL)`nKey: $($fileShareKey)`n"
       Ensure = "Present"
       Type = "File"
     }
-  }
 
-  Node "NginxWebServer" {
+    nxFile nginxphp_folder {
+      DestinationPath = "/var/www/html"
+      Type = "directory"
+      Recurse = $true
+      Force = $true
+    }
+
+    nxFile nginxconfig_folder {
+      DestinationPath = "/etc/nginx/sites-available"
+      Type = "directory"
+      Recurse = $true
+      Force = $true
+   }
+
     nxFile nginxphp {
       DestinationPath = "/var/www/html/index.php"
       Type = "file"
       Contents = $IndexPage
       Force = $true
+      DependsOn = "[nxFile]nginxphp_folder"
     }
 
-    nxFile nginxconfig
-    {
+    nxFile nginxconfig {
        SourcePath = "https://raw.githubusercontent.com/DCMattyG/azure-projects/master/vmss-linux-dsc/config/default"
        DestinationPath = "/etc/nginx/sites-available/default"
        Mode = "644"
        Type = "file"
        Force = $true
-       DependsOn = "[nxFile]nginxphp"
+       DependsOn = "[nxFile]nginxconfig_folder"
     }
 
     nxPackage php {
       Name = "php7.0-fpm"
       Ensure = "Present"
       PackageManager = "apt"
-      Arguments = "-o Dpkg::Options::='--force-confold'"
-      DependsOn = "[nxFile]nginxconfig"
+      DependsOn = "[nxFile]nginxphp", "[nxFile]nginxconfig"
     }
 
     nxPackage nginx {
       Name = "nginx"
       Ensure = "Present"
       PackageManager = "apt"
+      Arguments = "-o Dpkg::Options::='--force-confold'"
       DependsOn = "[nxPackage]php"
     }
-
-
 
     nxService nginxsvcstart {
       Name = "nginx"
       State = "running"
       Enabled = $true
       Controller = "systemd"
-      DependsOn = "[nxFile]nginxconfig", "[nxFile]nginxphp"
+      DependsOn = "[nxPackage]nginx"
     }
 
     # nxFirewall FWConfig {

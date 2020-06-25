41dff97860e9ff28234f0d5d1a274de0c52cc09a
Matthew Garrett <magarret@microsoft.com>
2020-06-24 18:37:36 -0700

    Added file download into script

--- dns-forwarder/scripts/bindConfig.sh
+++ dns-forwarder/scripts/bindConfig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-input="${1:1:-1}"
+input="${2:1:-1}"
 ip_list=""
 
 IFS=',' read -ra ADDR <<< "$input"
@@ -10,4 +10,6 @@ done
 
 ip_list="${ip_list:0:-2}"
 
+sudo wget "${1}scripts/named.conf.options" -O /etc/bind/named.conf.options
 sudo sed -i "s/.*{{IP}}.*/${ip_list}/" /etc/bind/named.conf.options
+sudo service bind9 restart

db58e339a53d50334cd1f4e5761c156ead0a38ab
Matthew Garrett <magarret@microsoft.com>
2020-06-24 18:30:35 -0700

    Added functionality to script and created cloud-init

--- dns-forwarder/scripts/bindConfig.sh
+++ dns-forwarder/scripts/bindConfig.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-for arg in "$@"
-do
-    echo "${arg}"
+input="${1:1:-1}"
+ip_list=""
+
+IFS=',' read -ra ADDR <<< "$input"
+for ip in "${ADDR[@]}"; do
+    ip_list="${ip_list}\t${ip};\n"
 done
+
+ip_list="${ip_list:0:-2}"
+
+sudo sed -i "s/.*{{IP}}.*/${ip_list}/" /etc/bind/named.conf.options
